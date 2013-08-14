2b95f4e3682d49b181cb7d31f8028eff65c6e080
Luis Polasek <lpolasek@gmail.com>
2013-08-14 15:57:15 -0300

    Updated models.

--- contab/models.py
+++ contab/models.py
@@ -13,22 +13,47 @@ class Cliente(models.Model):
 		return "%s %s" % (self.nombre, self.apellido)
 
 class Ejercicio(models.Model):
-	mes_inicio = models.PositiveSmallIntegerField()
-	anho_inicio = models.PositiveSmallIntegerField()
+	inicio = models.DateField()
 	formato_cuenta = models.CharField(max_length=19)
 	cliente = models.ForeignKey(Cliente)
 
 	def __unicode__(self):
 		return "%s %s (%d-%d)" % (self.cliente.nombre, self.cliente.apellido, self.mes_inicio, self.anho_inicio)
 		
-#class Cuenta(models.Model):
-	#nro_cuenta  (public)
-	#nombre_cuenta  (public)
-	#ajuste  (public)
-	
-	## lista_en_el_balance
-	#balance (public)
+class Cuenta(models.Model):
+	ejercicio = models.ForeignKey(Ejercicio)
+	numero = models.CharField(max_length=6)
+	nombre = models.CharField(max_length=40)
+	ajuste = models.BooleanField()
+
+	# lista_en_el_balance
+	balance = models.BooleanField()
+
+	def __unicode__(self):
+		return "%s-%s" % (self.numero, self.nombre)
+
 	##debito1  (public)
 	##credito_1  (public)
 	##debito_12  (public)
 	##credito_12  (public)
+
+class Asiento(models.Model):
+	ejercicio = models.ForeignKey(Ejercicio)
+	fecha = models.DateField()
+	
+RENGLON_TIPOS = (
+	( 'D', 'Debito' ),
+	( 'C', 'Credito' ),
+)
+
+
+class Renglon(models.Model):
+	asiento = models.ForeignKey(Asiento)
+	cuenta = models.ForeignKey(Cuenta)
+	detalle = models.CharField(max_length=35)
+	tipo = models.CharField(max_length=1, choices=RENGLON_TIPOS)
+	importe = models.IntegerField()
+
+	def __unicode__(self):
+		return "%s %s %s %s %d" % (self.cuenta.numero, self.asiento.fecha, self.detalle, self.tipo, self.importe)
+	
